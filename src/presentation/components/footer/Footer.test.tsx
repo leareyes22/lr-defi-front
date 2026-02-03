65e58d25f36606bd042e0f5490b19e87a57c0308
leandro-reyes-marvik <leandro.reyes@marvik.ai>
2026-02-03 14:11:05 -0300

    Fix build.

--- src/presentation/components/footer/Footer.test.tsx
+++ src/presentation/components/footer/Footer.test.tsx
@@ -1,4 +1,4 @@
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
 import { afterEach, describe, expect, it } from 'vitest';
 import { Footer } from './Footer';
 
@@ -6,16 +6,16 @@ describe('Footer Component', () => {
   afterEach(cleanup);
 
   it('Should render a footer element with the correct className.', () => {
-    render(<Footer />);
-    const element = screen.getByRole('contentinfo');
+    const { getByRole } = render(<Footer />);
+    const element = getByRole('contentinfo');
     expect(element.className).toEqual(
       'bg-teal-800 bg-opacity-30 p-2 m-2 rounded flex justify-center items-center'
     );
   });
 
   it('Should render a link to the Github repository', () => {
-    render(<Footer />);
-    const url = screen.getByRole('link').getAttribute('href');
+    const { getByRole } = render(<Footer />);
+    const url = getByRole('link').getAttribute('href');
     expect(url).toEqual('https://github.com/leareyes22');
   });
 });
